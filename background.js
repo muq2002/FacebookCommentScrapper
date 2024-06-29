728fd367cbf5a074d8c2149bcd57b20e97a43e5e
Muqtada Jabbar <mktad200@gmail.com>
2024-06-29 11:27:52 +0300

    make the extension only work on facebook

--- background.js
+++ background.js
@@ -1,26 +1,38 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content_script.js']
-    }).then(() => {
-      console.log('Content script injected successfully');
-    }).catch((error) => {
-      console.error('Error injecting content script:', error);
-    });
-  });
-  
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "openResultsTab") {
-      chrome.tabs.create({url: 'results.html'}, (newTab) => {
-        chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
-          if (tabId === newTab.id && info.status === 'complete') {
-            chrome.tabs.onUpdated.removeListener(listener);
-            chrome.tabs.sendMessage(tabId, {
-              action: "displayResults",
-              results: message.results
-            });
-          }
-        });
+  if (tab.url.startsWith("https://www.facebook.com")) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        files: ["content_script.js"],
+      })
+      .then(() => {
+        console.log("Content script injected successfully");
+      })
+      .catch((error) => {
+        console.error("Error injecting content script:", error);
       });
-    }
-  });
+  } else {
+    console.log("Extension is not designed to work on this site");
+    // Optionally, you can show a notification to the user
+    chrome.action.setPopup({
+      tabId: tab.id,
+      popup: "popup.html",
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "openResultsTab") {
+    chrome.tabs.create({ url: "results.html" }, (newTab) => {
+      chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+        if (tabId === newTab.id && info.status === "complete") {
+          chrome.tabs.onUpdated.removeListener(listener);
+          chrome.tabs.sendMessage(tabId, {
+            action: "displayResults",
+            results: message.results,
+          });
+        }
+      });
+    });
+  }
+});
